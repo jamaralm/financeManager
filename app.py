e9a1c902c354126b6bef9fcf791fd6abb18b5829
Joao Amaral <jamaralm07@gmail.com>
2025-02-18 21:06:59 -0300

    preparando para deploy render

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def summary():
     return render_template("summary.html", summary=summary_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
